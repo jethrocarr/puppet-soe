febf00bec52fe283415808cef4dfdd7014c769ff
Jethro Carr <jethro.carr@jethrocarr.com>
2016-09-03 23:53:06 +1200

    Added services files

---
+++ manifests/services.pp
@@ -0,0 +1,36 @@
+# Manage services
+class ::soe::services {
+  
+  # Disable unwanted/bad services
+  if ($::osfamily == "RedHat") {
+    service { ['avahi-daemon']:
+      ensure  => stopped,
+    }
+  }
+
+  # Define core services to allow them to recieve notifications
+  if ($::osfamily == "RedHat") {
+    service { 'sshd':
+      ensure => running,
+    }
+
+    if ($::operatingsystem != 'Fedora') {
+      service { 'rsyslog':
+        ensure => running,
+      }
+    }
+  }
+
+  if ($::osfamily == "Debian") {
+    service { 'sshd':
+      name   => 'ssh',
+      ensure => running,
+    }
+    service { 'rsyslog':
+      ensure => running,
+    }
+  }
+
+}
+
+# vi:smartindent:tabstop=2:shiftwidth=2:expandtab:
