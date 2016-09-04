c84f23fc1b884f68be532029918525a591f06771
Jethro Carr <jethro.carr@jethrocarr.com>
2016-09-04 22:08:59 +1200

    Ensure wheel group is created on Debian systems

--- manifests/manage/sudo.pp
+++ manifests/manage/sudo.pp
@@ -6,15 +6,27 @@ class soe::manage::sudo (
   ) {
 
   if ($enable) {
+
     class { '::sudo':
       purge               => false,
       config_file_replace => false,
     }
 
-    # TODO: is this FreeBSD compat?
     ::sudo::conf { 'admins':
       priority => 10,
       content  => "%wheel ALL=(ALL) NOPASSWD: ALL",
+    }
+
+    if ($::osfamily == 'Debian') {
+      # Debian systems don't have a wheel group created by default, but we want
+      # one in order to make our sudo rules functional. As this group is in the
+      # SOE module, it will be created before any other users outside of SOE,
+      # making it easy to rely on as a dependency.
+
+      group { 'wheel':
+        ensure => present,
+        system => true,
+      }
     }
   }
 }
